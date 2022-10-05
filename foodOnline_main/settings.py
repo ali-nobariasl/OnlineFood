c72d7eb9851595750e90e78df699a1f6f49b9a32
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-10-05 21:26:50 +0300

    AddingStaticFolder

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR /'static'
-STATICFILES_DIR = [
+STATIC_ROOT = BASE_DIR/'static'
+STATICFILES_DIRS = [
     'foodOnline_main/static'
 ]
 
