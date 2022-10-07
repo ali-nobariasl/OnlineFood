9f65e09d952ba61fb8a967844a5d02ecacd5b737
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-10-07 16:45:16 +0300

    AddingViewForAccount

--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import home
@@ -14,5 +14,6 @@ from .views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('accounts/', include('accounts.urls'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
