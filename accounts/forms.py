0c285158811d639205c2bccd00367cc9b0188e54
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-10-10 16:07:32 +0300

    FieldErrorsAndNonFieldErrors

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,3 +7,12 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields =['username', 'first_name', 'last_name', 'email', 'password', 'phone_number']
+   
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if confirm_password != password:
+            raise forms.ValidationError(
+                    "password does not match !!!"
+                )
