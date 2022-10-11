529bcbde593348f8785721c7b81dac53048f71ee
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-10-11 16:25:30 +0300

    FixxingVenderBugs

--- verndor/models.py
+++ verndor/models.py
@@ -5,7 +5,7 @@ from accounts.models import User , UserProfile
 
 class Vender(models.Model):
     user = models.OneToOneField(User, related_name="user", on_delete=models.CASCADE)
-    user_profile= models.OneToOneField(UserProfile, related_name="user", on_delete=models.CASCADE)   
+    user_profile= models.OneToOneField(UserProfile, related_name="userprofile", on_delete=models.CASCADE)   
     vender_name = models.CharField(max_length=50)
     vender_license = models.ImageField(upload_to='vendor/license')
     is_approved = models.BooleanField(default=False)
