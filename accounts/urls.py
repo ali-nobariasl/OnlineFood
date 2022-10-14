d0aae35a5e194c270edfa513f785dfbde6b175fd
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-10-14 12:39:13 +0300

    Login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from .views import registerUser, registerVender
+from .views import registerUser, registerVender ,login,logout,dashboard
 
 
 urlpatterns = [
     path('registerUser/',registerUser, name='registerUser'),
     path('registerVender/',registerVender, name='registerVender'),
+    
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+    path('dashboard/', dashboard, name='dashboard'),
+    
 ]
 
