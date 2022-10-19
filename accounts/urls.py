f1f2fc7478656f5e6219059a69bd6b8594be2c96
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-10-19 18:53:19 +0300

    passwordURLs

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import registerUser, registerVender ,login,logout,custDashboard,myAccount ,vendorDashboard , activate
+from .views import registerUser, registerVender ,login,logout,custDashboard,myAccount ,vendorDashboard , activate,reset_password_validate,forget_passwd,
 
 
 urlpatterns = [
@@ -14,6 +14,10 @@ urlpatterns = [
     path('vendorDashboard/', vendorDashboard, name='vendorDashboard'),
     
     path('activate/<uidb64>/<token>/', activate, name='activate'),
+    
+    path ('forget_passwword/', forget_passwd, name='forget_passwword'),
+    path ('reset_password_validate/<uidb64>/<token>', reset_password_validate, name='reset_password_validate'),
+    path ('reset_password/', reset_password, name='reset_password'),
     
 ]
 
