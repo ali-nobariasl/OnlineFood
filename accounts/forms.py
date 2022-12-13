6e94c995073183bce1f14526d3819450ac33e543
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-12-14 02:07:27 +0300

    a

--- accounts/forms.py
+++ accounts/forms.py
@@ -18,6 +18,11 @@ class UserForm(forms.ModelForm):
         
         
 class UserProfileForm(forms.ModelForm):
+    
+    profile_picture = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
+    cover_photo = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
+    
     class Meta:
         model= UserProfile
-        fields = "__all__"
+        fields = ['profile_picture', 'cover_photo','address_line_1','address_line_2','country','state','city',
+                  'pin_code', 'longitude','longitude' ]
--- vendor/views.py
+++ vendor/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render , get_object_or_404
-
+from django.shortcuts import render , get_object_or_404 , redirect
+from django.contrib import messages
 
 from .forms import VendorForm
 from accounts.forms import UserProfileForm
@@ -11,9 +11,22 @@ def vprofile(request):
     profile = get_object_or_404(UserProfile, user = request.user)
     vendor = get_object_or_404(Vendor, user = request.user)
     
+    if request.method == 'POST':
+        profile_form =UserProfileForm( request.POST, request.FILES , instance = profile)
+        vendor_form = VendorForm( request.POST, request.FILES , instance = vendor)
+        
+        if profile_form.is_valid() and vendor_form.is_valid():
+            profile_form.save()
+            vendor_form.save()
+            messages.success(request, 'settings updated successfully')
+            return redirect('vprofile')
+        else:
+            print(profile_form.errors)
+            print(vendor_form.errors)
+    else:
+        profile_form =UserProfileForm(instance = profile)
+        vendor_form = VendorForm(instance = vendor)
     
-    profile_form =UserProfileForm(instance = profile)
-    vendor_form = VendorForm(instance = vendor)
     context = {
         'profile_form': profile_form,
         'vendor_form': vendor_form,
