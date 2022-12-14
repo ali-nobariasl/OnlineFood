bbca235080acec6694e419f32919de63c7d1ec9d
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-12-14 13:04:13 +0300

    first way for making fields read only

--- accounts/forms.py
+++ accounts/forms.py
@@ -22,8 +22,9 @@ class UserProfileForm(forms.ModelForm):
     
     profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}), validators= [allow_only_image_validator])
     cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}), validators= [allow_only_image_validator] )
-    
+    latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+    longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
     class Meta:
         model= UserProfile
-        fields = ['profile_picture', 'cover_photo','address_line_1','address_line_2','country','state','city',
-                  'pin_code', 'longitude','longitude' ]
+        fields = ['profile_picture', 'cover_photo','address_line_1','address_line_2',
+                  'country','state','city','pin_code', 'latitude','longitude' ]
