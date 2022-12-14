d800507093c00402bf990776b3d6d02bb9e10a21
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-12-14 15:26:34 +0300

    configuring admin.py file for menu app and adding some example food items

--- menu/admin.py
+++ menu/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 
 from .models import Category ,FoodItem
 
-admin.site.register(Category)
-admin.site.register(FoodItem)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'sluge':('category_name',)}
+    list_display= ('category_name','vendor','updated')
+    search_fields = ('category_name','vendor__vendor_name')
+                          
+class FoodItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'sluge':('food_title',)}
+    list_display= ('food_title','category','vendor','price','is_available','updated')
+    search_fields = ('food_title','category__category_name','vendor__vendor_name','price')         
+    list_filter =('is_available',)                                   
+                          
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(FoodItem, FoodItemAdmin)
