cd1aa4912d5b89f0a149e4703e181863f50e2d84
Ali Nobariasl <freeeeboy.gh@gmail.com>
2022-12-19 14:51:10 +0300

    editing detail page

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 
 from vendor.models import Vendor
 
@@ -15,7 +15,8 @@ def marketplace(request):
 
 def vendor_detail(request, vendor_slug):
     
+    vendor = get_list_or_404(Vendor, vendor_slug=vendor_slug)
     context = {
-        
+        'vendor': vendor,
     }
     return render(request,'marketplace/vendor_detail.html', context)
